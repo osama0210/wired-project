435a83c4b8eea7746a5fb314f169b0ef1fc90c66
osas19 <osama.asmi04@gmail.com>
2024-02-19 11:42:49 +0100

    wired game website.

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ menuHamburger.addEventListener("click", () => {
 
 
 
+
+
+
+
+window.addEventListener("scroll", function () {
+  var navLinks = document.querySelector(".nav-links");
+  var logo = document.querySelector(".logo");
+  var sec2 = document.querySelector("#sec2");
+  var sec1 = document.querySelector("#sec1");
+
+  // Haal de positie van sectie 1 en sectie 2 op
+  var sec1Position = sec1.getBoundingClientRect().bottom;
+  var sec2Position = sec2.getBoundingClientRect().top;
+
+  // Als de onderkant van sectie 1 bovenaan of hoger is dan het scherm, verberg de navigatie en het logo
+  if (sec1Position <= 0) {
+    navLinks.style.display = "none";
+    logo.style.display = "none";
+  } else {
+    navLinks.style.display = "flex"; // Of de weergave-instelling die je normaal gebruikt
+    logo.style.display = "block"; // Of de weergave-instelling die je normaal gebruikt
+  }
+});
